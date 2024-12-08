b7bd19501790f1cd9c7c31d8066d9fa98da900e0
dido <jakkrapan.ckbl@gmail.com>
2024-12-08 17:09:30 +0700

    update popup pdf view

--- resources/views/livewire/popup_cityplan_files.blade.php
+++ resources/views/livewire/popup_cityplan_files.blade.php
@@ -33,13 +33,26 @@
                     <div class="tabs-menu4 w-100">
                         <nav class="nav border-bottom px-4 d-block d-lg-flex flex-2">
                             <a class="nav-link border border-bottom-0 py-1 br-5 mx-1 mx-md-1 active" data-bs-toggle="tab" href="#task-files">
-                                PDF ต้นฉบับ
+                                @if (is_null($job_imgs) || empty($job_imgs))
+                                    PDF ต้นฉบับ
+                                @else
+                                    <p style="font-weight: bold; color: blue;">PDF ต้นฉบับ</p>
+                                @endif
+                               
                             </a>
                             <a class="nav-link border border-bottom-0 py-1 br-5 mx-1 mx-md-1" data-bs-toggle="tab" href="#task-landindex">
-                                Word
+                                @if (is_null($landindex) || empty($landindex))
+                                    Word
+                                @else
+                                    <p style="font-weight: bold; color: blue;">Word</p>
+                                @endif
                             </a>
                             <a class="nav-link border border-bottom-0 py-1 br-5 mx-1 mx-md-1" data-bs-toggle="tab" href="#task-disclaim">
-                                PDF แนบรายงาน
+                                @if (is_null($disclaim) || empty($disclaim))
+                                    PDF แนบรายงาน
+                                @else
+                                    <p style="font-weight: bold; color: blue;">PDF แนบรายงาน</p>
+                                @endif
                             </a>
                             
                             
@@ -205,9 +218,9 @@
                                                         <td>
                                                             <div class="recent-files">
                                                                 <a data-bs-toggle="tooltip" data-bs-original-title="Open PDF" href="{{Storage::disk('s3')->url($job_img->file_path)}}" target="_blank">
-                                                                    @if (($job_img->file_type ?? '') === 'pdf')
+                                                                    @if ($job_img->file_type == 'pdf')
                                                                         <img class="img-responsive br-5" src="{{asset('images/icons8-pdf-96.png')}}" width="24" height="24" alt="Thumb-1">                                
-                                                                    @elseif (($job_img->file_type ?? '') === 'xlsx')
+                                                                    @elseif ($job_img->file_type == 'xlsx')
                                                                         <img class="img-responsive br-5" src="{{asset('images/icons8-xls-96.png')}}" width="24" height="24" alt="Thumb-1">
                                                                     @else
                                                                         <svg class="file-manager-icon w-icn me-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#b6dfff" d="M20,8.99969l-7-7H7a3,3,0,0,0-3,3v14a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3Z"/><path fill="#86cbff" d="M20 8.99969H15a2 2 0 0 1-2-2v-5zM19 22a.99974.99974 0 0 1-1-1V19a1 1 0 0 1 2 0v2A.99974.99974 0 0 1 19 22zM19 17a1.03391 1.03391 0 0 1-.71-.29.99108.99108 0 0 1-.21045-1.08984A1.14883 1.14883 0 0 1 18.29 15.29a1.02673 1.02673 0 0 1 .32959-.21.91433.91433 0 0 1 .76025 0 1.03418 1.03418 0 0 1 .33008.21 1.15772 1.15772 0 0 1 .21.33008A.98919.98919 0 0 1 19.71 16.71a1.15384 1.15384 0 0 1-.33008.21A.9994.9994 0 0 1 19 17zM15 18H9a1 1 0 0 1 0-2h6a1 1 0 0 1 0 2zM15 14H9a1 1 0 0 1 0-2h6a1 1 0 0 1 0 2zM10 10H9A1 1 0 0 1 9 8h1a1 1 0 0 1 0 2z"/></svg>
